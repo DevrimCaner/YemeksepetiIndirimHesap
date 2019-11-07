3151ba5b2f850e79631194e7e30a874db025c846
Devrim Caner ÖKDEM <DevrimCanerOkdem@outlook.com>
2019-11-07 17:56:43 +0300

    Final

--- main.js
+++ main.js
@@ -31,13 +31,13 @@ var app = new Vue({
             if(parseFloat(this.beforeTotal) < 30){
                 this.afterTotal = this.beforeTotal;
             }
-            else if(parseFloat(this.beforeTotal) >= 30){
+            else if(parseFloat(this.beforeTotal) >= 30 && parseFloat(this.beforeTotal) < 40){
                 this.afterTotal = parseFloat(this.beforeTotal) - 10;
             }
-            else if(parseFloat(this.beforeTotal) >= 40){
+            else if(parseFloat(this.beforeTotal) >= 40 && parseFloat(this.beforeTotal) < 70){
                 this.afterTotal = parseFloat(this.beforeTotal) - 20;
             }
-            else if(parseFloat(this.beforeTotal) >= 70){
+            else if(parseFloat(this.beforeTotal) >= 70 && parseFloat(this.beforeTotal) < 120){
                 this.afterTotal = parseFloat(this.beforeTotal) - 25;
             }
             else if(parseFloat(this.beforeTotal) >= 120){
@@ -50,7 +50,18 @@ var app = new Vue({
             //setting Prices
             this.prices.forEach(price => {
                 price.afterPrice = (parseFloat(price.beforePrice) / parseFloat(this.beforeTotal)) * parseFloat(this.afterTotal);
+                price.afterPrice = parseFloat(price.afterPrice) * 100;
+                price.afterPrice = Math.floor(parseFloat(price.afterPrice));
+                price.afterPrice = parseFloat(price.afterPrice) / 100;
             });
-        }
+        },
+        removeElement : function(index){
+            this.prices.splice(index, 1);
+        },
+        clearData : function (){
+            this.prices = [{ beforePrice: 0, afterPrice: 0, piece: 1}]
+            this.beforeTotal = 0;
+            this.afterTotal = 0 ;
+        } 
     }
 });
